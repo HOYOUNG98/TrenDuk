a032f61b333313b27304caf51290c5e926b9b97b
Kevin <kevin16727@gmail.com>
2020-12-26 18:35:38 +0900

    database model created

--- serviceNew/index.py
+++ serviceNew/index.py
@@ -1,6 +1,11 @@
 from scraper.scraper import scrapeGamePage, scrapeLinksPage
+from model.Gibo import Gibo
+from model.Node import Node
 
 if __name__ == "__main__":
     links = scrapeLinksPage(pageOptions=(1, 1))
 
     parsedData = scrapeGamePage(links[0])
+    
+
+    #connect('GiboDB', host=config.DB_URL)
