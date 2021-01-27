b57566c58406ef79f1fed651b3e6f0cfb0fbbc3d
Kevin <kevin16727@gmail.com>
2021-01-27 22:47:56 +0900

    Next Chakra WGo

---
+++ new-web/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script type="text/javascript" src="/wgo/wgo.min.js"></script>
+        </Head>
+        <body>
+          {/* Make Color mode to persists when you refresh the page. */}
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
