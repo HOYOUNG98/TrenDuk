c942dd0420f9d35a9ad6dcf8f879d2ceb1a1984b
Kevin <kevin16727@gmail.com>
2021-03-31 22:37:26 +0900

    field upper case fix

--- cron/helper/scraper.py
+++ cron/helper/scraper.py
@@ -3,29 +3,27 @@ import requests
 import bs4
 from pymongo import MongoClient
 
-'''
+"""
     In cyberoro page, we can mainly consider 2 types of links
     1. page that contains links of games (10 games max)
     2. page that shows the game
     This file will mainly have functions working around these two types of pages.
-'''
+"""
 
-client = MongoClient(
-    "mongodb+srv://kevin4163:ghdi4163@trenduk.sucyo.mongodb.net/trenduk?retryWrites=true&w=majority")
-gibo = client['trenduk']['gibo']
+client = MongoClient("mongodb+srv://kevin4163:ghdi4163@trenduk.sucyo.mongodb.net/trenduk?retryWrites=true&w=majority")
+gibo = client["trenduk"]["gibo"]
 
 
 def request(link):
     response = requests.get(link)
-    decoded_content = bs4.BeautifulSoup(
-        response.content.decode('euc-kr', 'replace'), "html.parser")
+    decoded_content = bs4.BeautifulSoup(response.content.decode("euc-kr", "replace"), "html.parser")
     return decoded_content
 
 
-'''
+"""
     Find (at max) 10 links inside content. Check for duplicates from database.
     If duplicate found, return with False as second value
-'''
+"""
 
 
 def parse_links(content):
@@ -34,7 +32,7 @@ def parse_links(content):
     links = []
     for div in link_divs:
         link = div.find("a")["href"][21:-2]
-        link = link[1:(link.find(",")-1)]
+        link = link[1 : (link.find(",") - 1)]
 
         count = gibo.count_documents({"link": link})
         if count > 0:
@@ -55,43 +53,42 @@ def parse_game(content, link):
     info = info.replace("[", ":").replace("\r\n", "")
     split_info = info.split("]")
 
-    gibo = {"Time": {}, "BlackPlayer": {},
-            "WhitePlayer": {}, "Moves": [], "Link": link}
+    gibo = {"time": {}, "blackPlayer": {}, "whitePlayer": {}, "moves": [], "link": link}
     for category in split_info:
         if len(category) <= 2:
             continue
 
         field = category[:2]
         if field == "TE":
-            gibo["Title"] = category[3:]
+            gibo["title"] = category[3:]
         elif field == "RD":
-            gibo["Date"] = category[3:]
+            gibo["date"] = category[3:]
         elif field == "PC":
-            gibo["Location"] = category[3:]
+            gibo["location"] = category[3:]
         elif field == "TM":
-            gibo["Time"]["Given"] = category[3:]
+            gibo["time"]["given"] = category[3:]
         elif field == "LT":
-            gibo["Time"]["Seconds"] = category[3:]
+            gibo["time"]["seconds"] = category[3:]
         elif field == "LC":
-            gibo["Time"]["Count"] = category[3:]
+            gibo["time"]["count"] = category[3:]
         elif field == "KO":
-            gibo["Komi"] = category[3:]
+            gibo["komi"] = category[3:]
         elif field == "RE":
-            gibo["Result"] = category[3:]
+            gibo["result"] = category[3:]
         elif field == "PB":
-            gibo["BlackPlayer"]["Name"] = category[3:]
+            gibo["blackPlayer"]["name"] = category[3:]
         elif field == "BR":
-            gibo["BlackPlayer"]["Rank"] = category[3:]
+            gibo["blackPlayer"]["rank"] = category[3:]
         elif field == "PW":
-            gibo["WhitePlayer"]["Name"] = category[3:]
+            gibo["whitePlayer"]["name"] = category[3:]
         elif field == "WR":
-            gibo["WhitePlayer"]["Rank"] = category[3:]
+            gibo["whitePlayer"]["rank"] = category[3:]
         elif field == "HD":
-            gibo["Handicap"] = category[3:]
+            gibo["handicap"] = category[3:]
         else:
             print("Undefined category:", category)
 
-    moves = content[end_semi_colon+1:]
+    moves = content[end_semi_colon + 1 :]
     end_parenthesis = moves.find(")")
 
     if end_parenthesis == -1:
@@ -103,9 +100,9 @@ def parse_game(content, link):
 
     for split_move in split_moves:
         move = {}
-        move["Color"] = split_move[:1]
-        move["Move"] = split_move[1:]
-        gibo["Moves"].append(move)
+        move["color"] = split_move[:1]
+        move["move"] = split_move[1:]
+        gibo["moves"].append(move)
 
     return gibo
 
