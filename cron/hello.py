08ffc1e4e234b981aa7b3227438de07816df6f0c
Kevin <kevin16727@gmail.com>
2021-05-13 23:36:48 +0900

    conversion to using csv

--- cron/hello.py
+++ cron/hello.py
@@ -1,90 +1,117 @@
 import time
 from flask import Flask
 from pymongo import MongoClient
-from pandas import DataFrame
+from pandas import DataFrame, json_normalize
 import os
 
 from helper.scraper import request, parse_links, parse_game
 from helper.analyzer import assort_corners, build_tree, tree_to_list
-from helper.database import fetchAllGibos, insertManyGibos, insertManyNodes, upsertManyNodes
 
 app = Flask(__name__)
 
 os.environ["PYTHONHASHSEED"] = "TrendukSecret"
 
-MAX_PAGE = 465
+MAX_PAGE = 500
 
 
 @app.cli.command()
-def scheduled():
-    """Run scheduled job."""
-    for page in range(1, 5):
+def fetchGames():
+
+    gibo_df = []
+    for page in range(1, MAX_PAGE):
         link = "https://www.cyberoro.com/bcast/gibo.oro?param=1&div=1&Tdiv=B&Sdiv=2&pageNo={0}&blockNo=1".format(page)
         content = request(link)
-        links, duplicate_found = parse_links(content)
+        links = parse_links(content)
 
-        if duplicate_found == False and len(links) == 0:
-            print("Reached Endpoint")
-            break
-
-        gibo_objects = []
         for link in links:
-            content = request(link)
-            gibo_object = parse_game(content, link)
+            gibo_object = parse_game(link)
 
             if gibo_object == None:
                 continue
-
-            gibo_objects.append(gibo_object)
-
+            gibo_df.append(gibo_object)
+        print("page {0}".format(page), len(gibo_df))
         page += 1
 
-        root = {"depth": 0, "children": []}
-        for gibo_object in gibo_objects:
-            corners = assort_corners(gibo_object["moves"])
-            for moves in corners:
-                root = build_tree(root, moves, gibo_object)
+    gibo_df = DataFrame(gibo_df)
 
-        node_list = tree_to_list(root)
+    # Rearrange columns so moves go to end
+    cols = gibo_df.columns.tolist()
+    cols = cols[0:3] + cols[4:] + [cols[3]]
+    gibo_df = gibo_df[cols]
 
-        insertManyGibos(gibo_objects)
-        upsertManyNodes(node_list)
-
-        if duplicate_found == False:
-            print("Duplicate Found: Ending Process")
-            break
+    gibo_df.to_csv("output.csv", index=False, encoding="utf-8-sig")
 
 
-@app.cli.command()
-def test1():
-    link = "https://www.cyberoro.com/bcast/gibo.oro?param=1&div=1&Tdiv=B&Sdiv=2&pageNo=1&blockNo=1"
-    content = request(link)
-    links, _ = parse_links(content)
+# @app.cli.command()
+# def scheduled():
+#     """Run scheduled job."""
+#     for page in range(1, MAX_PAGE):
+#         link = "https://www.cyberoro.com/bcast/gibo.oro?param=1&div=1&Tdiv=B&Sdiv=2&pageNo={0}&blockNo=1".format(page)
+#         content = request(link)
+#         links, duplicate_found = parse_links(content)
 
-    gibo_documents = []
+#         if duplicate_found == False and len(links) == 0:
+#             print("Reached Endpoint")
+#             break
 
-    for link in links[:2]:
-        content = request(link)
-        gibo_document = parse_game(content, link)
+#         gibo_objects = []
+#         for link in links:
+#             content = request(link)
+#             gibo_object = parse_game(content, link)
 
-        gibo_documents.append(gibo_document)
+#             if gibo_object == None:
+#                 continue
 
-    root = {"depth": 0, "children": []}
-    for gibo_document in gibo_documents:
-        corners = assort_corners(gibo_document["moves"])
-        for moves in corners:
-            root = build_tree(root, moves, gibo_document)
+#             gibo_objects.append(gibo_object)
 
-    node_list = tree_to_list(root)
+#         page += 1
 
-    df = DataFrame(node_list)
-    df["len"] = df["data"].str.len()
-    print(df.sort_values(by=["len"], ascending=False))
+#         root = {"depth": 0, "children": []}
+#         for gibo_object in gibo_objects:
+#             corners = assort_corners(gibo_object["moves"])
+#             for moves in corners:
+#                 root = build_tree(root, moves, gibo_object)
 
-    insertManyGibos(gibo_documents)
-    upsertManyNodes(node_list)
-    result = upsertManyNodes(node_list)
-    print(result)
+#         node_list = tree_to_list(root)
+
+#         insertManyGibos(gibo_objects)
+#         upsertManyNodes(node_list)
+
+#         if duplicate_found == False:
+#             print("Duplicate Found: Ending Process")
+#             break
+
+
+# @app.cli.command()
+# def test1():
+#     link = "https://www.cyberoro.com/bcast/gibo.oro?param=1&div=1&Tdiv=B&Sdiv=2&pageNo=1&blockNo=1"
+#     content = request(link)
+#     links, _ = parse_links(content)
+
+#     gibo_documents = []
+
+#     for link in links[:2]:
+#         content = request(link)
+#         gibo_document = parse_game(content, link)
+
+#         gibo_documents.append(gibo_document)
+
+#     root = {"depth": 0, "children": []}
+#     for gibo_document in gibo_documents:
+#         corners = assort_corners(gibo_document["moves"])
+#         for moves in corners:
+#             root = build_tree(root, moves, gibo_document)
+
+#     node_list = tree_to_list(root)
+
+#     df = DataFrame(node_list)
+#     df["len"] = df["data"].str.len()
+#     print(df.sort_values(by=["len"], ascending=False))
+
+#     insertManyGibos(gibo_documents)
+#     upsertManyNodes(node_list)
+#     result = upsertManyNodes(node_list)
+#     print(result)
 
 
 @app.cli.command()
