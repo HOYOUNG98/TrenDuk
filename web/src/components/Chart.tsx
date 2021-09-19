7ca4ec49c9b171f6c857f342797e842deef2ccdf
Kevin <kevin16727@gmail.com>
2021-09-18 21:16:24 -0500

    now using csv

--- web/src/components/Chart.tsx
+++ web/src/components/Chart.tsx
@@ -1,16 +1,19 @@
 // library imports
 import React from "react";
 import {
-  XYPlot,
-  VerticalBarSeries,
   HorizontalGridLines,
   VerticalGridLines,
   XAxis,
   YAxis,
+  FlexibleWidthXYPlot,
+  LineSeries,
+  ChartLabel,
+  AreaSeries,
 } from "react-vis";
-import { IReactVisData } from "../types";
+import "react-vis/dist/style.css";
 
 // local imports
+import { IReactVisData } from "../types";
 
 interface ChartProps {
   data: Array<IReactVisData>;
@@ -18,21 +21,49 @@ interface ChartProps {
   move: string;
 }
 
-export const Chart: React.FC<ChartProps> = ({ data }) => {
+const referenceData = [...Array(14).keys()].map((i) => {
+  return { x: 2008 + i, y: 50 };
+});
+
+export const Chart: React.FC<ChartProps> = ({ data, variant }) => {
   return (
     <div>
-      <XYPlot
+      <FlexibleWidthXYPlot
         height={300}
-        width={800}
         yDomain={[0, 100]}
-        xDomain={[2007.8, 2021.2]}
+        xDomain={[2008, 2021]}
       >
-        <VerticalBarSeries data={data} barWidth={0.5} color={"black"} />
         <XAxis />
         <YAxis />
         <HorizontalGridLines />
         <VerticalGridLines />
-      </XYPlot>
+        <LineSeries data={data} color={"black"} style={{ strokeWidth: 4 }} />
+        <AreaSeries data={data} color={"rgba(188, 185, 185, 0.6)"} />
+        <LineSeries
+          data={referenceData}
+          color={"8f8f8f"}
+          strokeStyle={"dashed"}
+        />
+        <ChartLabel
+          text="Year"
+          className="alt-x-label"
+          includeMargin={false}
+          xPercent={0.025}
+          yPercent={1.01}
+        />
+
+        <ChartLabel
+          text={variant}
+          className="alt-y-label"
+          includeMargin={false}
+          xPercent={0.03}
+          yPercent={0.06}
+          style={{
+            transform: "rotate(-90)",
+            textAnchor: "end",
+          }}
+        />
+      </FlexibleWidthXYPlot>
     </div>
   );
 };
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,7 +7,16 @@ import { WGoBoard } from "../components/WGoBoard";
 import { Chart } from "../components/Chart";
 import { RootState } from "../store";
 import { NavBar } from "../components/NavBar";
-import { HStack, Radio, RadioGroup, Wrap } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  HStack,
+  Radio,
+  RadioGroup,
+  VStack,
+  Wrap,
+} from "@chakra-ui/react";
 import { ReactVisDataReducer } from "../helpers/rechartConversion";
 import { IReactVisData } from "../types";
 
@@ -32,35 +41,82 @@ const Index: React.FC = () => {
     );
   }, [hoverPoint, currentMoves]);
 
-  const dispatch = useDispatch();
   return (
-    <>
+    <Flex direction="column">
       <NavBar />
-      <HStack margin={5} spacing="20px">
-        <Wrap>
-          <WGoBoard />
-          <RadioGroup
-            value={selectedColor === "B" ? "1" : "2"}
-            onChange={() => {
-              dispatch({ type: "SELECT_COLOR" });
-            }}
+      <Flex
+        width="100%"
+        height="100%"
+        marginTop="-50px"
+        justifyContent="center"
+        alignItems="center"
+        direction={["column", "column", "row", "row"]}
+      >
+        {/* Moves List */}
+        <Flex
+          width={["100%", "160px", "160px", "160px"]}
+          height={["100px", "500px", "500px", "500px"]}
+          direction={["row", "row", "column", "column"]}
+          overflowX="auto"
+        >
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="xl">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+          <Box borderWidth="1px" height="100px" width="160px" borderRadius="lg">
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+          <Box
+            borderWidth="1px"
+            height="100px"
+            width="160pxs"
+            borderRadius="lg"
           >
-            <HStack spacing={5}>
-              <Radio colorScheme="black" value="1">
-                흑
-              </Radio>
-              <Radio colorScheme="white" value="2">
-                백
-              </Radio>
-            </HStack>
-          </RadioGroup>
-        </Wrap>
-        <Wrap marginRight="20px">
-          <Chart data={yearlyPick} move={hoverPoint} variant={"선택률"} />
-          <Chart data={yearlyWin} move={hoverPoint} variant={"승률"} />
-        </Wrap>
-      </HStack>
-    </>
+            <Text fontSize="large">MOVE</Text>
+            <Text fontSize="medium">Description</Text>
+          </Box>
+        </Flex>
+        {/* Board */}
+        <WGoBoard />
+        {/* Charts */}
+        <Flex
+          width={["100%", "40%", "40%", "40%"]}
+          height={["100px", "500px", "500px", "500px"]}
+          direction={["row", "row", "column", "column"]}
+          overflow="auto"
+        >
+          <Chart
+            data={yearlyPick}
+            move={hoverPoint}
+            variant={"Pick Rate (%)"}
+          />
+          <Chart data={yearlyWin} move={hoverPoint} variant={"Win Rate (%)"} />
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
 
