64295ebd42932325a5b6bfd41cae87affaddfaa1
Kevin <kevin16727@gmail.com>
2022-03-20 16:18:32 -0500

    parser: parsing sequences

--- server/TrenDukUtil/type.py
+++ server/TrenDukUtil/type.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 class TreeNode:
-    def __init__(self, move:str, color:str, depth:int, game_id:int) -> None:
-        self.id: int = hash((move, color, depth))
+    def __init__(self, move:str, color:str, sequence_depth:int, game_depth: int, game_id:int) -> None:
+        self.id: int = hash((move, color, sequence_depth))
         self.move = move
         self.color = color
-        self.depth = depth
+        self.sequence_depth = sequence_depth
+        self.game_depth = game_depth
         self.games: list[int] = [game_id]
         self.children: set[int] = set()
     
@@ -21,8 +22,16 @@ class TreeNode:
     
     def existsChild(self, node_id:int) -> bool:
         return node_id in self.children
+    
+    @classmethod
+    def root(cls):
+        return cls('root', 'root', 0, 0, -1)
+    
+    def __repr__(self):
+        return self.color + self.move
+    
 
 
-class GameInfo:
-    def __init__(self, game_id) -> None:
-        self.id = game_id
+# class GameInfo:
+#     def __init__(self, *args, **kwargs) -> None:
+        
